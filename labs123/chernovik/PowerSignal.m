a7018a0b1952e1a197ebee59189a082600b336e4
sofyafyaa <89632493+sofyafyaa@users.noreply.github.com>
2024-03-23 16:00:24 +0300

    remove unneswcesery files

--- labs123/chernovik/PowerSignal.m
+++
@@ -1,8 +0,0 @@
-function P_Signal = PowerSignal(Signal)
-% Calculate average Power of Signal
-
-S_real2 = Signal .* conj(Signal);
-P_Signal = sum(S_real2) / length(Signal);
-
-end
-
--- labs123/chernovik/file_reader.m
+++
@@ -1,7 +0,0 @@
-function bits = file_reader(file, filesize)
-            fileID = fopen(file,'r','b');
-            bitstream = fread(fileID, filesize, 'ubit1'); % Считали QAM_Cells*Bit_Depth бит из файла
-            fclose('all');
-            bits = reshape(bitstream,1,[]);
-end
-
